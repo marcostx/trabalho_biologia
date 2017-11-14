39f0a84b2e2c182204a20188c5e3fd2567033f95
ecclesiedei <ecclesiedei@gmail.com>
2017-11-13 22:43:01 -0200

    refactoring + rede recorrente funcionando

--- baseline.py
+++ baseline.py
@@ -3,6 +3,7 @@ from Bio import Seq, SeqIO, SeqRecord
 import itertools
 from parser import load_csv
 import numpy as np
+import argparse
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten
 from keras.constraints import maxnorm
@@ -14,51 +15,10 @@ import itertools
 from sklearn.metrics import accuracy_score,precision_score,recall_score,f1_score
 from keras.utils import np_utils
 from keras.layers.recurrent import LSTM
+from keras.layers import SimpleRNN
+from keras import initializers
+from utils import *
 
-
-def create_dict_words():
-	kmer=3
-	nucleotides = ['a', 't', 'g', 'c']
-	words=[]
-	binary_words={}
-	combinations = itertools.product(*itertools.repeat(nucleotides, 3))
-	for idx,j in enumerate(combinations):
-	    words.append(''.join(j))
-	    _ = np.zeros((len(nucleotides)*len(nucleotides)*len(nucleotides)))
-	    _[idx] = 1.0
-	    binary_words[''.join(j)]=_
-
-	return binary_words
-
-
-def twin(km):
-    return Seq.reverse_complement(km)
-
-def kmers(seq,k):
-	kmer=[]
-	for i in range(len(seq)-k+1):
-		kmer.append(seq[i:i+k])
-
-	return kmer
-
-
-def binary_representation(fn,k=3,limit=1):
-    d = collections.defaultdict(int)
-    dict_words = create_dict_words()
-    
-    seq_l = fn
-    kms = kmers(seq_l,k)
-
-    representation=[]
-    for idx in range(0,len(kms)-1):
-    	f_ =  np.array(dict_words[kms[idx]])
-    	s_ = np.array(dict_words[kms[idx+1]])
-
-    	representation.append(np.append(f_,s_))
-
-    
-    
-    return np.array(representation)
 
 def create_baseline_model(num_classes,inp_shape):
 	
@@ -81,35 +41,37 @@ def create_baseline_model(num_classes,inp_shape):
 
 def create_reccurent_model(num_classes,inp_shape):
 	epochs = 120
+	batch_size=32
+	lrate=0.001
+	decay = lrate/epochs
 
 	model = Sequential()
-	model.add(LSTM(32, return_sequences=True,
-               input_shape=(inp_shape[2], inp_shape[1])))  # returns a sequence of vectors of dimension 32
-	model.add(LSTM(32, return_sequences=True))  # returns a sequence of vectors of dimension 32
-	model.add(LSTM(32))  # return a single vector of dimension 32
+	model.add(LSTM(32, return_sequences=False,
+               input_shape=inp_shape[1:]))
 	model.add(Dense(num_classes, activation='softmax'))
-
+	
+	sgd = Adam(lr=lrate, epsilon=1e-08, decay=decay)
 	model.compile(loss='categorical_crossentropy',
-	              optimizer='rmsprop',
+	              optimizer=sgd,
 	              metrics=['accuracy'])
 	
 
 	return model, epochs
 
 if __name__ == '__main__':
-	assert len(sys.argv[1]) > 1
 
-	X,y = load_csv(sys.argv[1])
+	#assert len(sys.argv[1]) > 1
+	parser = argparse.ArgumentParser()
+	parser.add_argument('-i', '--input_dataset', help='input file', required=True)
+	parser.add_argument('-m', '--method', help='method', required=True)
+	ARGS = parser.parse_args()
+
+
+	X,y = load_csv(ARGS.input_dataset)
 	
 
-	#a='ACCGATTATGCA'
-	X_binarized = [ binary_representation(item) for item in X]
-	X_binarized = np.array(X_binarized)
-
-	X_binarized = X_binarized.reshape((X_binarized.shape[0],X_binarized.shape[1],X_binarized.shape[2],1))
-	inp_shape = X_binarized.shape[1:]
-	print(inp_shape)
-	exit()
+	X_binarized = preprocess(X)
+	inp_shape = X_binarized.shape
 	
 
 	X_train, X_test, y_train, y_test = train_test_split(X_binarized, y, test_size=0.33, random_state=42)
@@ -119,11 +81,14 @@ if __name__ == '__main__':
 
 	num_classes = y_train.shape[1]
 
-	#model,epochs = create_baseline_model(num_classes,inp_shape)
-	model,epochs = create_reccurent_model(num_classes,inp_shape)
+	if ARGS.method == 'baseline':
+		model,epochs = create_baseline_model(num_classes,inp_shape)
+	elif ARGS.method == 'recurrent':
+		model,epochs = create_reccurent_model(num_classes,inp_shape)
 
 	seed = 7
 	np.random.seed(seed)
+	
 	model.fit(X_train, y_train, validation_data=(X_test, y_test_), nb_epoch=epochs, batch_size=50)
 	# Final evaluation of the model
 	pred = model.predict(X_test, verbose=0)
---
+++ utils.py
@@ -0,0 +1,72 @@
+import collections, sys
+from Bio import Seq, SeqIO, SeqRecord
+import itertools
+from parser import load_csv
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.constraints import maxnorm
+from keras.optimizers import SGD
+from keras.layers import Conv2D, MaxPooling2D
+from keras.optimizers import Adam
+from sklearn.model_selection import train_test_split
+import itertools
+from sklearn.metrics import accuracy_score,precision_score,recall_score,f1_score
+from keras.utils import np_utils
+from keras.layers.recurrent import LSTM
+from keras.layers import SimpleRNN
+from keras import initializers
+
+
+def preprocess(X):
+	X_binarized = [ binary_representation(item) for item in X]
+	X_binarized = np.array(X_binarized)
+
+	X_binarized = X_binarized.reshape((X_binarized.shape[0],X_binarized.shape[1],1))
+
+	return X_binarized
+
+def create_dict_words():
+	kmer=3
+	nucleotides = ['a', 't', 'g', 'c']
+	words=[]
+	binary_words={}
+	combinations = itertools.product(*itertools.repeat(nucleotides, 3))
+	for idx,j in enumerate(combinations):
+	    words.append(''.join(j))
+	    _ = np.zeros((len(nucleotides)*len(nucleotides)*len(nucleotides)))
+	    _[idx] = 1.0
+	    binary_words[''.join(j)]=_
+
+	return binary_words
+
+
+def twin(km):
+    return Seq.reverse_complement(km)
+
+def kmers(seq,k):
+	kmer=[]
+	for i in range(len(seq)-k+1):
+		kmer.append(seq[i:i+k])
+
+	return kmer
+
+
+def binary_representation(fn,k=3,limit=1):
+    d = collections.defaultdict(int)
+    dict_words = create_dict_words()
+    
+    seq_l = fn
+    kms = kmers(seq_l,k)
+
+    representation=[]
+    for idx in range(0,len(kms)-1):
+    	f_ =  np.array(dict_words[kms[idx]])
+    	s_ = np.array(dict_words[kms[idx+1]])
+
+    	representation.append(np.append(f_,s_))
+
+    representation=np.array(representation).flatten()
+    
+    
+    return np.array(representation)
