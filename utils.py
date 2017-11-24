7b90b5b63594f70a348aeac53309808ddcea2840
Marcos <ecclesiedei@gmail.com>
2017-11-24 15:24:48 -0200

    resultados parciais + alteracoes de validacao

--- utils.py
+++ utils.py
@@ -159,7 +159,9 @@ def get_binary_words(vector_sentences):
 
             word_rep = np.array(word_rep)
             words.append(word_rep.flatten())
-
+        if not (len(words) == 498):
+            print(sentence)
+            
         sentences.append(words)
     sentences = np.array(sentences)
 
